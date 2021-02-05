afb2f6629f9a9cde9021ab1c3702122359ceb299
Your Name <ebelinggianmarco@gmail.com>
2021-02-05 15:55:17 +0100

    Prepare deployment

---
+++ backend/data/products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    name: "Kumiko Canvas Print",
+    image: "/images/1.jpg",
+    description:
+      "Each Pittoresco canvas is produced using a 12 colour giclée fine art printing process. They're hand finished by experienced canvas framing teams. Ready to hang straight out the box.",
+    brand: "Frank Moth",
+    category: "Abstracts",
+    price: 9.99,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Beach I Canvas Print",
+    image: "/images/2.jpg",
+    description:
+      " Each Pittoresco canvas is produced using a 12 colour giclée fine art printing process. They're hand finished by experienced canvas framing teams. Ready to hang straight out the box.",
+    brand: "Hecetu",
+    category: "Nature",
+    price: 9.99,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Go Home Canvas Print",
+    image: "/images/3.jpg",
+    description:
+      "Each Pittoresco canvas is produced using a 12 colour giclée fine art printing process. They're hand finished by experienced canvas framing teams. Ready to hang straight out the box.",
+    brand: "Andy Westface",
+    category: "Abstracts",
+    price: 9.99,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Best Friends Canvas Print",
+    image: "/images/4.jpg",
+    description:
+      "Each Pittoresco canvas is produced using a 12 colour giclée fine art printing process. They're hand finished by experienced canvas framing teams. Ready to hang straight out the box.",
+    brand: "Maarten Léon",
+    category: "Abstracts",
+    price: 9.99,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Tiger Canvas Print",
+    image: "/images/5.jpg",
+    description:
+      "Each Pittoresco canvas is produced using a 12 colour giclée fine art printing process. They're hand finished by experienced canvas framing teams. Ready to hang straight out the box.",
+    brand: "Be Martt",
+    category: "Abstracts",
+    price: 9.99,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Somewhere Canvas Print",
+    image: "/images/6.jpg",
+    description:
+      "Each Pittoresco canvas is produced using a 12 colour giclée fine art printing process. They're hand finished by experienced canvas framing teams. Ready to hang straight out the box.",
+    brand: "Andy Westface",
+    category: "Abstracts",
+    price: 9.99,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
